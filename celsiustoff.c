a4903a39fa3bd2f28a83404be1202b50852c77de
Rashminani <124244392+Rashminani@users.noreply.github.com>
2023-02-03 21:09:38 +0530

    Add files via upload

---
+++ celsiustoff.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define con 1.8
+int main()
+{float c,f ;
+printf("enter the celcsius value");
+scanf(" %f,&c");
+f=(con*c)+32,
+printf("the fahrenheite value is %f",f);
+return(0); }
+
+
+
+
+
