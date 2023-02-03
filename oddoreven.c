a3b7c21099577e85aee94f3f33bb4e887e75797e
Rashminani <124244392+Rashminani@users.noreply.github.com>
2023-02-03 21:35:39 +0530

    Add files via upload

---
+++ oddoreven.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{int num;
+printf("enter a num ");
+//logic
+if(num%2==0){
+	printf("the given number is even",num);
+}
+else(num%2=!0);
+	printf("the given number is odd ",num);
+}
+//return(0);
+getch();
+}
+	
+
+
