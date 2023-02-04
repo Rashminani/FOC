9191bac69a141b607c57177dae64cc352a5ae599
Rashminani <124244392+Rashminani@users.noreply.github.com>
2023-02-04 10:50:41 +0530

    Add files via upload

---
+++ valuen.c.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,j,n,s,terms;
+	//clrscr();
+	printf("Enter the value n:");
+	s=0;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+		}
+		s=s;
+	}
+	printf("sum of the series s=%d.",s);
+	getch();
+}
